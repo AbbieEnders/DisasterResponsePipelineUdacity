7dffbba49d58a2e3b7cd155314d4886fdffcaddf
Abbie Enders <abigailenders19@gmail.com>
2024-06-09 20:57:00 -0400

    Update process_data.py

--- data/process_data.py
+++ data/process_data.py
@@ -43,7 +43,7 @@ def clean_data(df):
 
 
 def save_data(df, database_filename):
-    engine = create_engine(str(database_filename))
+    engine = create_engine(('sqlite:///{}'.format(database_filename)))
     df.to_sql('DisasterResponseClean', engine, index=False, if_exists = "replace")
 
 
